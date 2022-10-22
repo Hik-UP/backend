c61de1a67e55a6ae9a1a83bb1215aa1a89dae98f
NeantBetaS <71332802+NeantBetaS@users.noreply.github.com>
2022-10-22 07:46:12 +0000

    add(rateLimiter): allow 100 requests per 15 minutes.

---
+++ server/api/src/middleware/rateLimiter.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterMemory, IRateLimiterOptions } from 'rate-limiter-flexible';
+
+import { logger } from '../logger';
+
+function createRateLimiterMemory(): RateLimiterMemory {
+  const opts: IRateLimiterOptions = {
+    points: 100,
+    duration: 15 * 60
+  };
+
+  return new RateLimiterMemory(opts);
+}
+
+function rateLimiter(req: Request, res: Response, next: NextFunction): void {
+  rateLimiterMemory
+    .consume(req.ip)
+    .then(() => {
+      logger.info(`Request ${req.method} ${req.url} allowed`);
+      next();
+    })
+    .catch(() => {
+      logger.warn(`Request ${req.method} ${req.url} blocked`);
+      res.status(429).json({
+        error: 'Too Many Requests'
+      });
+    });
+}
+
+const rateLimiterMemory = createRateLimiterMemory();
+
+export { rateLimiter };
