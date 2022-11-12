fb2e9ac388714fba0a2c8f8318062108bf472fdd
NeantBetaS <71332802+NeantBetaS@users.noreply.github.com>
2022-11-12 18:58:43 +0000

    refactor(scripts): fix docker permissions.

--- server/api/node-init.sh
+++ server/api/node-init.sh
@@ -17,66 +17,24 @@ prisma_env_generator() {
   /tmp/.secrets/db/db)\"" > /usr/prisma/.env
 }
 
-generate_signup_jwt_keys() {
+credentials_generator_jwt() {
   local readonly PASS="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 128)"
 
-  if ! [ -f "/tmp/.secrets/jwt/signup.privkey.pem" ] 		||
-  ! [ -f "/tmp/.secrets/jwt/signup.pubkey.pem" ]; then
-    printf "${PASS}" > /tmp/.secrets/jwt/signup.privkey.passphrase
+  if ! [ -f "/tmp/jwt.privkey.pem" ] 				||
+  ! [ -f "/tmp/jwt.pubkey.pem" ]; then
+    printf "${PASS}" > /tmp/jwt.privkey.passphrase
     openssl genpkey						\
     -pass pass:"${PASS}"					\
     -algorithm RSA						\
     -aes256							\
     -pkeyopt rsa_keygen_bits:4096				\
-    -out /tmp/.secrets/jwt/signup.privkey.pem
+    -out /tmp/jwt.privkey.pem
     openssl rsa							\
     -passin pass:"${PASS}"					\
-    -in /tmp/.secrets/jwt/signup.privkey.pem			\
+    -in /tmp/jwt.privkey.pem					\
     -pubout							\
     -outform PEM						\
-    -out /tmp/.secrets/jwt/signup.pubkey.pem
-  fi
-}
-
-generate_login_jwt_keys() {
-  local readonly PASS="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 128)"
-
-  if ! [ -f "/tmp/.secrets/jwt/login.privkey.pem" ] 		||
-  ! [ -f "/tmp/.secrets/jwt/login.pubkey.pem" ]; then
-    printf "${PASS}" > /tmp/.secrets/jwt/login.privkey.passphrase
-    openssl genpkey						\
-    -pass pass:"${PASS}"					\
-    -algorithm RSA						\
-    -aes256							\
-    -pkeyopt rsa_keygen_bits:4096				\
-    -out /tmp/.secrets/jwt/login.privkey.pem
-    openssl rsa							\
-    -passin pass:"${PASS}"					\
-    -in /tmp/.secrets/jwt/login.privkey.pem			\
-    -pubout							\
-    -outform PEM						\
-    -out /tmp/.secrets/jwt/login.pubkey.pem
-  fi
-}
-
-generate_user_jwt_keys() {
-  local readonly PASS="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 128)"
-
-  if ! [ -f "/tmp/.secrets/jwt/user.privkey.pem" ] 		||
-  ! [ -f "/tmp/.secrets/jwt/user.pubkey.pem" ]; then
-    printf "${PASS}" > /tmp/.secrets/jwt/user.privkey.passphrase
-    openssl genpkey						\
-    -pass pass:"${PASS}"					\
-    -algorithm RSA						\
-    -aes256							\
-    -pkeyopt rsa_keygen_bits:4096				\
-    -out /tmp/.secrets/jwt/user.privkey.pem
-    openssl rsa							\
-    -passin pass:"${PASS}"					\
-    -in /tmp/.secrets/jwt/user.privkey.pem			\
-    -pubout							\
-    -outform PEM						\
-    -out /tmp/.secrets/jwt/user.pubkey.pem
+    -out /tmp/jwt.pubkey.pem
   fi
 }
 
@@ -97,9 +55,7 @@ main() {
   yarn
   prisma_env_generator
   mkdir /tmp/.secrets/jwt
-  generate_signup_jwt_keys
-  generate_login_jwt_keys
-  generate_user_jwt_keys
+  credentials_generator_jwt
   prisma_migrate "$@"
   yarn $@
 }
